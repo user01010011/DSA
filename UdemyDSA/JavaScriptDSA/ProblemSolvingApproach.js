bb7566033a1529fbb81050f52b1ba77857dd8e2f
Shasha Yan <shashayan@gmail.com>
2022-06-16 18:20:39 -0500

    update

---
+++ UdemyDSA/JavaScriptDSA/ProblemSolvingApproach.js
@@ -0,0 +1,14 @@
+// Write a function which takes two numbers and return their sum.
+
+// 1. Can I restate the problem in my own words? 
+    // - "implement addition" or "implement adding two numbers"
+// 2. What are the inputs that go into the problem? 
+    // - integers? floats/floating points? How large are these numbers going to be? What about string for really large numbers? 
+    // - Are we only working with 2 inputs? Is it always 2? What if someone wants to add 4, 10 numbers, is it valid to add 10 numbers together mathematically as our function only going to work with 2 numbers?
+// 3. What are the outputs that should come from the solution to the problem? 
+    //  - integer? float? string? 
+// 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
+    // - Do we have enough information? What happens if someone only passes one number? Then we don't have enough information to do addition at that point. 
+    // - If/when we don't have enough information, do we use 0 or do we return undefined or null or something? Or ignore it all together? What do we do? 
+// 5. How should I label the important pieces of data that are a part of the problem?
+    // - maybe name the function Add2Nums, num1 and num2 are the arguments, and sum is the result that we return. 
