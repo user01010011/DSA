74018ae0501bda56af2e33dc9e7338769d151b87
Shasha Yan <shashayan@gmail.com>
2022-06-18 11:09:37 -0500

    add problem

---
+++ UdemyDSA/JavaScriptDSA/Recursion_Easy_Power.js
@@ -0,0 +1,21 @@
+// Recursion 
+
+// Easy 
+
+// power 
+
+// Write a function called power which accepts a base and an exponent. The function should return the power of the base to the exponent. This function should mimic the functionality of Math.pow() - do not worry about negative bases and exponents.
+
+// Examples: 
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+// Solution: 
+function power(base, exp) {
+    if (exp === 0) return 1; 
+    return base * power(base, exp - 1)
+}
+
+// Time Complexity = O(n) // O(exp)
+// Space Complexity = O(n)
