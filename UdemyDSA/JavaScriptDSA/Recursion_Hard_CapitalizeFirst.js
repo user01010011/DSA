cd0a5ba528b3d519be91413c857fbe238585ecc7
Shasha Yan <shashayan@gmail.com>
2022-06-18 18:56:23 -0500

    update solution

--- UdemyDSA/JavaScriptDSA/Recursion_Hard_CapitalizeFirst.js
+++ UdemyDSA/JavaScriptDSA/Recursion_Hard_CapitalizeFirst.js
@@ -1,31 +1,36 @@
-// Recursion 
+// Recursion
 
-// Hard 
+// Hard
 
-// capitalizeFirst 
+// capitalizeFirst
 
 // Write a recursive function called capitalizeFirst. Given an array of strings, capitalize the first letter of each string in the array.
 
-// Example: 
+// Example:
 // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
 
-// Solution 1 - 1: 
+// Solution 1 - 1:
 function capitalizeFirst(arr) {
-    if (arr.length === 1) {
-        return [arr[0][0].toUpperCase().concat(arr[0].slice(1))];
-    }
-    let result = capitalizeFirst(arr.slice(0, -1)); 
-    let capLastStr = (arr[arr.length - 1][0]).toUpperCase().concat(arr[arr.length - 1].slice(1)); 
-    result.push(capLastStr); 
-    return result;
+  if (arr.length === 1) {
+    return [arr[0][0].toUpperCase().concat(arr[0].slice(1))];
+  }
+  let result = capitalizeFirst(arr.slice(0, -1));
+  let capLastStr = arr[arr.length - 1][0]
+    .toUpperCase()
+    .concat(arr[arr.length - 1].slice(1));
+  result.push(capLastStr);
+  return result;
 }
 
-// Solution 1 - 2: 
-function capitalizeWords(array) {
-    if (array.length === 1) {
-        return [array[0].toUpperCase()]; 
-    }
-    let result = capitalizeWords(array.slice(0, -1)); 
-    result.push(array.slice(array.length - 1)[0].toUpperCase())
-    return result; 
+// Solution 1 - 2:
+function capitalizeFirst(array) {
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr[1]];
+  }
+  const res = capitalizeFirst(array.slice(0, -1));
+  const string =
+    array.slice(array.length - 1)[0][0].toUpperCase() +
+    array.slice(array.length - 1)[0].substr(1);
+  res.push(string);
+  return res;
 }
