b009cdeef923ae49d3d9630b2a0639be700651f0
Shasha Yan <shashayan@gmail.com>
2022-06-18 12:50:08 -0500

    add problem

---
+++ UdemyDSA/JavaScriptDSA/Recursion_Easy_ProductOfArray.js
@@ -0,0 +1,16 @@
+// Recursion 
+
+// Easy  
+
+// productOfArray 
+
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+
+// Examples: 
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+function productOfArray(arr) {
+    if (arr.length === 0) return 1; 
+    return arr[0] * productOfArray(arr.splice(1))
+}
