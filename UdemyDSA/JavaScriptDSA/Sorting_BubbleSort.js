3335da38b2b72c7280f81a7c80c9d9fcadfa49ab
Shasha Yan <shashayan@gmail.com>
2022-06-19 00:57:53 -0500

    add example

--- UdemyDSA/JavaScriptDSA/Sorting_BubbleSort.js
+++ UdemyDSA/JavaScriptDSA/Sorting_BubbleSort.js
@@ -34,3 +34,41 @@ function bubbleSort(arr) {
   return arr;
 }
 bubbleSort([37, 45, 29, 12, 88, -3]);
+
+
+// Optimized with noSwaps - ES5: 
+function bubbleSort(arr) {
+    let noSwaps;  // default set to no swaps
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true; 
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j]; 
+                arr[j] = arr[j+1]; 
+                arr[j+1] = temp; 
+                noSwaps = false; 
+            }
+        }
+        if (noSwaps) break; 
+    }
+    return arr; 
+}
+
+// Optimized with noSwaps - ES2015
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+    let noSwaps; 
+    for (let i = arr.length; i > 0; i--) {
+      for (let j = 0; j < i - 1; j++) {
+        noSwaps = true; 
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+          noSwaps = false; 
+        }
+      }
+      if (noSwaps) false; 
+    }
+    return arr;
+  }
