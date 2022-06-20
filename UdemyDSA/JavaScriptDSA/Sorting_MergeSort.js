029a733aacafb553463aebcf16672d42d70bf475
Shasha Yan <shashayan@gmail.com>
2022-06-20 06:47:52 -0500

    add example

--- UdemyDSA/JavaScriptDSA/Sorting_MergeSort.js
+++ UdemyDSA/JavaScriptDSA/Sorting_MergeSort.js
@@ -6,15 +6,16 @@ function merge(arr1, arr2) {
     let results = []; 
     let i = 0; 
     let j = 0; 
-    while (i < arr1.length && j < arr2.length) {
+    while (i < arr1.length && j < arr2.length) { // while there is still data for us to look at in both arrays
         if (arr2[j] > arr1[i]) {
-            results.push(arr1[i]); 
-            i++; 
+            results.push(arr1[i]); // we put the smaller item into the array 
+            i++; // then we move the pointer i to the next item in arr1
         } else {
             results.push(arr2[j]);
-            j++; 
+            j++; // then move the pointer j to the next item in arr2
         }
-    }
+    } 
+    // once we exhausted / looped through one array, if we have not finished the other array, we take the rest of that array and put it in to the results array.
     while (i < arr1.length) {
        results.push(arr1[i]) 
        i++; 
@@ -28,11 +29,11 @@ function merge(arr1, arr2) {
 // merge([100], [1, 2, 3, 5, 6])
 
 function mergeSort(arr) {
-    if (arr.length <= 1) return arr; 
-    let mid = Math.floor(arr.length/2); 
-    let left = mergeSort(arr.slice(0, mid)); 
-    let right = mergeSort(arr.slice(mid)); 
-    return merge(left, right); 
+    if (arr.length <= 1) return arr; // our base case of the recursion
+    let mid = Math.floor(arr.length/2); // split the array in half, the whole integer 
+    let left = mergeSort(arr.slice(0, mid)); // index 0 to index mid-1 // recursion - call mergeSort function (go up to the mergeSort on top) again, then go through left & right again  
+    let right = mergeSort(arr.slice(mid)); // goes from mid to the end of the array 
+    return merge(left, right); // call the merge function at the very top, marge the base cases first, then go back through the call stack 
 }
 
 mergeSort([10, 24, 76, 73, 72, 1, 9])
--- UdemyDSA/JavaScriptDSA/Sorting_QuickSort.js
+++ UdemyDSA/JavaScriptDSA/Sorting_QuickSort.js
@@ -1,3 +1,61 @@
 // Sorting 
 
 // Quick Sort
+
+// ES2015 
+function pivot(arr, start=0, end=arr.length-1) {
+    const swap = (arr, idx1, idx2)          => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]; 
+    };; 
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start]; 
+    let swapIdx = start; 
+    for (let i = start + 1; i <= end; i++) {
+        if (pivot > arr[i]) {
+            swapIdx++; 
+            swap(arr, swapIdx, i); 
+        }
+    }
+    // Swap the pivot from the start the swapPoint 
+    swap(arr, start, swapIdx); 
+    return swapIdx; 
+}
+
+function quickSort(arr, left=0, right=arr.length-1) { // no start and end because we are going to call portions of the array
+    if (left < right){
+        let pivotIndex = pivot(arr, left, right) 
+        // left 
+        quickSort(arr, left, pivotIndex-1); 
+        // right 
+        quickSort(arr, pivotIndex+1, right); 
+    }
+    return arr; 
+
+}
+
+// ES5
+function pivot(arr, start=0, end=arr.length+1) {
+    function swap(array, i, j) {
+        var temp = array[i]; 
+        array[i] = array[j]; 
+        array[j] = temp; 
+    }
+    var pivot = arr[start]; 
+    var swapIdx = start; 
+    for (var i = start + 1; i < arr.length; i++) { // start + 1 b/c no need to compare it to itself
+        if (pivot > arr[i]) {
+            swapIdx++; 
+            swap(arr, swapIdx, i)
+        }
+    }
+    swap(arr, start, swapIdx) // not swapping pivot because pivot is the value of the start index
+    return swapIdx; 
+}
+// pivot([4, 8, 2, 1, 5, 7, 6, 3])
+// [4, 8, 2, 1, 5, 7, 6, 3] // pivot is start=0, which is 4; swapIdx is at start=0, aslo 4; for loop i starts at start + 1; 
+// [4, 2, 8, 1, 5, 7, 6, 3]
+// [4, 2, 8, 1, 5, 7, 6, 3]
+// [4, 2, 1, 8, 5, 7, 6, 3]
+// [4, 2, 1, 3, 5, 7, 6, 8]
+
+// [3, 2, 1, 4, 5, 7, 6, 8]
