83552ab912617e0eea1dd5a0c72a404a2c0c310d
Shasha Yan <shashayan@gmail.com>
2022-06-19 23:57:00 -0500

    add example

--- UdemyDSA/JavaScriptDSA/Sorting_MergeSort.js
+++ UdemyDSA/JavaScriptDSA/Sorting_MergeSort.js
@@ -25,8 +25,14 @@ function merge(arr1, arr2) {
     }
     return results; 
 }
-merge([100], [1, 2, 3, 5, 6])
+// merge([100], [1, 2, 3, 5, 6])
 
-// function mergeSort() {
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr; 
+    let mid = Math.floor(arr.length/2); 
+    let left = mergeSort(arr.slice(0, mid)); 
+    let right = mergeSort(arr.slice(mid)); 
+    return merge(left, right); 
+}
 
-// }
+mergeSort([10, 24, 76, 73, 72, 1, 9])
