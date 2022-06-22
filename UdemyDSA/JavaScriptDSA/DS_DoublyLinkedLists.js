6ba37c76b0dcce7d311c6579c0bace7f81dceea3
Shasha Yan <shashayan@gmail.com>
2022-06-22 09:41:13 -0500

    add example

--- UdemyDSA/JavaScriptDSA/DS_DoublyLinkedLists.js
+++ UdemyDSA/JavaScriptDSA/DS_DoublyLinkedLists.js
@@ -88,6 +88,17 @@ class DoublyLinkedList {
     this.length++; 
     return this; // return entire list 
   }
+  // un-optimized version
+  get(index) {
+    if (index < 0 || index >= this.length) return null; // recall that index counts at 0, so last item should be length - 1; 
+    var count = 0; 
+    var current = this.head; 
+    while(count != index) {
+        current = current.next; 
+        count++; 
+    }
+    return current; 
+  }
 
 }
 var list = new DoublyLinkedList();
