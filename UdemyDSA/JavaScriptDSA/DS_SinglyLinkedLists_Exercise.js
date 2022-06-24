9b233b9bda76012356cd688c47faaf7e60b75aaa
Shasha Yan <shashayan@gmail.com>
2022-06-23 23:29:04 -0500

    add problem

--- UdemyDSA/JavaScriptDSA/DS_SinglyLinkedLists_Exercise.js
+++ UdemyDSA/JavaScriptDSA/DS_SinglyLinkedLists_Exercise.js
@@ -7,6 +7,10 @@
 // This function should take in a value and add a node to the end of the SinglyLinkedList.
 // It should return the SinglyLinkedList.
 
+// pop
+// This function should remove a node at the end of the SinglyLinkedList.
+// It should return the node removed.
+
 class Node {
     constructor(val) {
         this.val = val; 
@@ -33,5 +37,32 @@ class SinglyLinkedList {
         }
         this.length++; 
         return this; 
+    }
+
+    push(val) {
+        if (!this.head) this.head = newNode; 
+        else this.tail.next = newNode; 
+        this.tail = newNode; 
+        return this; 
+    }
+    pop() {
+        if (!this.head) return undefined; 
+        let removedNode; 
+        if (this.head === this.tail) {
+            removedNode = this.head; 
+            this.head = null; 
+            this.tail = null; 
+        } else {
+            let current = this.head; 
+            let newTail = current; 
+            while (current.next) {
+                newTail = current; 
+                current = current.next; 
+            }
+            this.tail = newTail; 
+            this.tail.next = null; 
+        }
+        this.length--; 
+        return removedNode; 
     }
 }
