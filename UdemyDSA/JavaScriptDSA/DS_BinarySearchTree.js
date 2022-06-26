a7de51528690b5ece085e8b86a164b3ebfd43d39
Shasha Yan <shashayan@gmail.com>
2022-06-25 23:57:43 -0500

    add example

--- UdemyDSA/JavaScriptDSA/DS_BinarySearchTree.js
+++ UdemyDSA/JavaScriptDSA/DS_BinarySearchTree.js
@@ -1,59 +1,71 @@
-// Data Structures 
+// Data Structures
 
 // Binary Search Tree
 class Node {
-    constructor(value) {
-        this.left = null; 
-        this.right = null; 
-        this.value = value; 
-    }
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
 }
 class BinarySearchTree {
-    constructor() {
-        this.root = null; 
-    }
-    insert(value) {
-        let newNode = new Node(value); 
-        if (this.root === null) {
-            this.root = newNode; 
-            return this; 
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined; // if there is a duplicate
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
         } else {
-            let current = this.root; 
-            while(true) {
-                if (value === current.value) return undefined; // if there is a duplicate 
-                if (value < current.value) {
-                    if (current.left === null) {
-                        current.left = newNode; 
-                        return this; 
-                    } else {
-                        current = current.left;
-                    }
-                } else if (value > current.value) {
-                    if (current.right === null) {
-                        current.right = newNode; 
-                        return this; 
-                    }
-                } else {
-                    current = current.right; 
-                }
-            }
+          current = current.right;
         }
+      }
     }
+  }
 
-
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return current;
+  }
 }
 
-
-
-let tree = new BinarySearchTree(); 
-// tree.root = new Node(10); 
-// tree.root.right = new Node(15); 
-// tree.root.left = new Node(7); 
-// tree.root.left.right = new Node(9); 
+let tree = new BinarySearchTree();
+// tree.root = new Node(10);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
 // tree.insert(10)
 // tree.insert(5)
 // tree.insert(13)
-// tree.insert(11) 
+// tree.insert(11)
 // tree.insert(2)
 // tree.insert(16)
-// tree.insert(7) 
+// tree.insert(7)
