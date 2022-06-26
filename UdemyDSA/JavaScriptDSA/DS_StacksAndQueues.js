12f210fc22c5cc8d2d6994b9c27aca006c947996
Shasha Yan <shashayan@gmail.com>
2022-06-25 21:30:03 -0500

    add example

--- UdemyDSA/JavaScriptDSA/DS_StacksAndQueues.js
+++ UdemyDSA/JavaScriptDSA/DS_StacksAndQueues.js
@@ -28,4 +28,14 @@ class Stack {
         }
         return ++this.size; 
     }
+    pop() {
+        if (!this.first) return null; 
+        let temp = this.first; 
+        if (this.first === this.last) {
+            this.last = null; 
+        }
+        this.first = this.first.next; 
+        this.size--; 
+        return temp.value; 
+    }
 }
