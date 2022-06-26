0236896967460f33e429eb0bc2daef018a640da6
Shasha Yan <shashayan@gmail.com>
2022-06-25 23:51:23 -0500

    add example

--- UdemyDSA/JavaScriptDSA/DS_BinarySearchTree.js
+++ UdemyDSA/JavaScriptDSA/DS_BinarySearchTree.js
@@ -1,13 +1,6 @@
 // Data Structures 
 
 // Binary Search Tree
-
-class BinarySearchTree {
-    constructor() {
-        this.root = null; 
-    }
-}
-
 class Node {
     constructor(value) {
         this.left = null; 
@@ -15,9 +8,51 @@ class Node {
         this.value = value; 
     }
 }
+class BinarySearchTree {
+    constructor() {
+        this.root = null; 
+    }
+    insert(value) {
+        let newNode = new Node(value); 
+        if (this.root === null) {
+            this.root = newNode; 
+            return this; 
+        } else {
+            let current = this.root; 
+            while(true) {
+                if (value < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode; 
+                        return this; 
+                    } else {
+                        current = current.left;
+                    }
+                } else if (value > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode; 
+                        return this; 
+                    }
+                } else {
+                    current = current.right; 
+                }
+            }
+        }
+    }
+
+
+}
+
+
 
 let tree = new BinarySearchTree(); 
-tree.root = new Node(10); 
-tree.root.right = new Node(15); 
-tree.root.left = new Node(7); 
-tree.root.left.right = new Node(9); 
+// tree.root = new Node(10); 
+// tree.root.right = new Node(15); 
+// tree.root.left = new Node(7); 
+// tree.root.left.right = new Node(9); 
+// tree.insert(10)
+// tree.insert(5)
+// tree.insert(13)
+// tree.insert(11) 
+// tree.insert(2)
+// tree.insert(16)
+// tree.insert(7) 
