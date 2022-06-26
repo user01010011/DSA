47c2160f0d136ac4c8bcfbfd34406255b8c0f253
Shasha Yan <shashayan@gmail.com>
2022-06-26 00:01:45 -0500

    add example

--- UdemyDSA/JavaScriptDSA/DS_BinarySearchTree.js
+++ UdemyDSA/JavaScriptDSA/DS_BinarySearchTree.js
@@ -57,6 +57,22 @@ class BinarySearchTree {
     return current;
   }
 
+  contains(value) {
+    if (this.root === null) return false; 
+    let current = this.root; 
+    let found = false; 
+    while(current && !found) {
+        if (value < current.value) {
+            current = current.left; 
+        } else if (value > current.value) {
+            current = current.right; 
+        } else {
+            return true; 
+        }
+    }
+    return false; 
+  }
+
 }
 
 let tree = new BinarySearchTree();
