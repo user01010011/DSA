74a996f95260d6e8bf1f65bc65793cd0bd7310e2
Shasha Yan <shashayan@gmail.com>
2022-06-25 21:51:30 -0500

    add example

--- UdemyDSA/JavaScriptDSA/DS_StacksAndQueues.js
+++ UdemyDSA/JavaScriptDSA/DS_StacksAndQueues.js
@@ -39,3 +39,29 @@ class Stack {
         return temp.value; 
     }
 }
+
+class Queue {
+    constructor() {
+        this.first = null; 
+        this.last = null; 
+        this.size = 0; 
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value; 
+        this.next = null; 
+    } 
+    enqueue(val) {
+        let newNode = new Node(val); 
+        if (!this.first) {
+            this.first = newNode; 
+            this.last = newNode; 
+        } else {
+            this.last.next = newNode; 
+            this.last = newNode; 
+        }
+        return ++this.size; 
+    }
+}
