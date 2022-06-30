49fb7fc4d3677b7e5d107a2c2b7be779e872c072
Shasha Yan <shashayan@gmail.com>
2022-06-29 22:40:58 -0500

    add exercise

--- UdemyDSA/JavaScriptDSA/BinaryHeap_MaxBinaryHeap.js
+++ UdemyDSA/JavaScriptDSA/BinaryHeap_MaxBinaryHeap.js
@@ -31,4 +31,34 @@ class MaxBinaryHeap {
         } 
         return max; 
     }
+
+    sinkDown() { // <-- you can put index in, but empty will start from the top 
+        let idx = 0; 
+        const length = this.values.length; 
+        const element = this.values[0];
+        while(true) {
+            let leftChildIdx = 2 * idx + 1; 
+            let rightChildIdx = 2 * idx + 2; 
+            let leftChild, rightChild; 
+            let swap = null; 
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx]; 
+                if (leftChild > element){
+                    swap = leftChildIdx; 
+                }
+            } 
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx]; 
+                if ((swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild) 
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break; 
+            this.values[idx] = this.values[swap]; 
+            this.values[swap] = element; 
+            idx = swap; 
+        }
+    }
 }
