a829a6ffa3086587187cfc9e29d7993a2e1a6e0a
Shasha Yan <shashayan@gmail.com>
2022-06-29 22:10:31 -0500

    add exercise

--- UdemyDSA/JavaScriptDSA/BinarySearchTrees.js
+++ UdemyDSA/JavaScriptDSA/BinarySearchTrees.js
@@ -56,4 +56,18 @@ class BinarySearchTree {
         if (!found) return undefined; 
         return current; 
     }
+
+    BFS() {
+        let node = this.root,
+        data = [], // what will return at the end 
+        queue = []; // empty array 
+        queue.push(node); // place the root in the queue; 
+        while (queue.length) { // as long as something is in the queue, we remove from the beginning
+            node = queue.shift(); 
+            data.push(node.value); 
+            if (node.left) queue.push(node.left); // if there is a node on the left, we push it into the queue 
+            if (node.right) queue.push(node.right); 
+        }
+        return data; 
+    }
 }
