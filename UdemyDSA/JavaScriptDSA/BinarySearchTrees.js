176f520744d9ab743b0c1cd42ce6a46280914b3f
Shasha Yan <shashayan@gmail.com>
2022-06-29 22:07:24 -0500

    add exercise

--- UdemyDSA/JavaScriptDSA/BinarySearchTrees.js
+++ UdemyDSA/JavaScriptDSA/BinarySearchTrees.js
@@ -40,4 +40,20 @@ class BinarySearchTree {
         }
     }
 
+    find(value) {
+        if (this.root === null) return false; 
+        let current = this.root; 
+        let found = false; 
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left; 
+            } else if (value > current.value) {
+                current = current.right; 
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return undefined; 
+        return current; 
+    }
 }
