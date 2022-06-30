07e33cb6d6936d1cfe7f3a3b6bb6df565cff7d30
Shasha Yan <shashayan@gmail.com>
2022-06-29 22:36:21 -0500

    add exercise

--- UdemyDSA/JavaScriptDSA/BinaryHeap_MaxBinaryHeap.js
+++ UdemyDSA/JavaScriptDSA/BinaryHeap_MaxBinaryHeap.js
@@ -3,10 +3,12 @@ class MaxBinaryHeap {
         // this.values = [41, 39, 33, 18, 27, 12]; 
         this.values = [100, 79, 52, 41, 33, 12];
     }
+
     insert(element) {
         this.values.push(element); 
         this.bubbleUp(); 
     }
+
     bubbleUp() {
         let idx = this.values.length - 1; 
         const element = this.values[idx];      
@@ -18,5 +20,15 @@ class MaxBinaryHeap {
             this.values[idx] = parent; 
             idx = parentIdx; 
         }
+    }
+
+    extractMax() {
+        const max = this.values[0]; // first store the value of max 
+        const end = this.values.pop(); //  pop the last item off 
+        if (this.values.length > 0) {
+            this.values[0] = end; // put the end node to the top 
+            this.sinkDown(); 
+        } 
+        return max; 
     }
 }
