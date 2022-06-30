79b66412c9d6632cd9145f2555fba6960622ae8c
Shasha Yan <shashayan@gmail.com>
2022-06-29 22:55:28 -0500

    add exercise

--- UdemyDSA/JavaScriptDSA/BinaryHeap_PriorityQueue_MinBinaryHeap.js
+++ UdemyDSA/JavaScriptDSA/BinaryHeap_PriorityQueue_MinBinaryHeap.js
@@ -6,6 +6,25 @@ class PriorityQueue {
     constructor() {
         this.values = []; 
     }
+
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority); 
+        this.values.push(newNode); 
+        this.bubbleUp(); 
+    }
+
+    bubbleUp() {
+        let idx = this.values.length - 1; 
+        const element = this.values[idx]; 
+        while(idx > 0) {
+            let parentIdx = Math.floor((idx - 1) / 2); 
+            let parent = this.values[parentIdx]; 
+            if (element.priority <= parent.priority) break; 
+            this.values[parentIdx] = element; 
+            this.values[idx] = parent; 
+            idx = parentIdx; 
+        }
+    }
 }
 
 class Node {
