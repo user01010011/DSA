b4c7f82c6f29954b9c0916730d942213c1ca0738
Shasha Yan <shashayan@gmail.com>
2022-06-30 23:37:55 -0500

    add exercise

--- UdemyDSA/JavaScriptDSA/DS_HashTables_HashSetGet.js
+++ UdemyDSA/JavaScriptDSA/DS_HashTables_HashSetGet.js
@@ -25,14 +25,33 @@ function hash(key, arrayLen) {
     return total; 
 }
 
-_hash(key) {
-    let total = 0; 
-    let WEIRD_PRIME = 31; 
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i]; 
-        let value = char.charCodeAt(0) - 96; 
-        total = (total * WEIRD_PRIME + value) % this.keyMap.length; 
+class HashTable {
+    constructor(size = 5) {
+        this.keyMap = new Array(size); 
     }
-    return total; 
+
+    _hash(key) {
+        let total = 0; 
+        let WEIRD_PRIME = 31; 
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]; 
+            let value = char.charCodeAt(0) - 96; 
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length; 
+        }
+        return total; 
+    }
+    
+    set(key, value) {
+        let index = this._hash(key); 
+        return index; 
+    }
+    
+    get(key) {
+    
+    }
+
 }
 
+
+let ht = new HashTable(); 
+ht.set("hello world", "goodbye!!")
