94a9f71fe4cd66d34c29d3a3cd45f78c0aea64c7
Shasha Yan <shashayan@gmail.com>
2022-06-30 23:39:55 -0500

    update

--- UdemyDSA/JavaScriptDSA/DS_HashTables_HashSetGet.js
+++ UdemyDSA/JavaScriptDSA/DS_HashTables_HashSetGet.js
@@ -1,60 +1,59 @@
-// Data Structures 
+// Data Structures
 
 // Hash Tables
 
 // Example of a Hash Function:
 function hash(key, arrayLen) {
-    let total = 0; 
-    for (let char of key) {
-        // map "a" to 1, "b" to 2, "c" to 3, etc. 
-        let value = char.charCodeAt(0) - 96; 
-        total = (total + value) % arrayLen; 
-    }
-    return total; 
+  let total = 0;
+  for (let char of key) {
+    // map "a" to 1, "b" to 2, "c" to 3, etc.
+    let value = char.charCodeAt(0) - 96;
+    total = (total + value) % arrayLen;
+  }
+  return total;
 }
 
 // Improved Hash Function - runtime, more random, etc.
 function hash(key, arrayLen) {
-    let total = 0; 
-    let WEIRD_PRIME = 31; 
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i]; 
-        let value = char.charCodeAt(0) - 96;
-        total = (total * WEIRD_PRIME + value) % arrayLen; 
-    }
-    return total; 
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+  return total;
 }
 
 class HashTable {
-    constructor(size = 5) {
-        this.keyMap = new Array(size); 
-    }
+  constructor(size = 5) {
+    this.keyMap = new Array(size);
+  }
 
-    _hash(key) {
-        let total = 0; 
-        let WEIRD_PRIME = 31; 
-        for (let i = 0; i < Math.min(key.length, 100); i++) {
-            let char = key[i]; 
-            let value = char.charCodeAt(0) - 96; 
-            total = (total * WEIRD_PRIME + value) % this.keyMap.length; 
-        }
-        return total; 
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
     }
-    
-    set(key, value) {
-        let index = this._hash(key); 
-        if (!this.keyMap[index]) {
-            this.keyMap[index] = []; 
-        }
-        this.keyMap[index].push([key, value]); 
-    }
-    
-    get(key) {
-    
-    }
+    return total;
+  }
 
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+
+  get(key) {}
 }
 
-
-let ht = new HashTable(); 
-ht.set("hello world", "goodbye!!")
+let ht = new HashTable();
+ht.set("hello world", "goodbye!!");
+ht.set("dogs", "are cool");
+ht.set("cats", "are fine");
+ht.set("i love", "pizza");
